a4a33e5e5be4eeae20e7c0b08759f6908e6ef866
Jacobiscoolio <Jacobiscoolio@JacobiscoolioPc.attlocal.net>
2016-03-10 21:06:14 -0600

    Test Revision

---
+++ src/test_revisions/Class3.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test_revisions;
+
+/**
+ *
+ * @author Jacobiscoolio
+ */
+public class Class3 {
+    private int u;
+    private int v;
+    private int w;
+    
+    Class3(int d, int e, int f){
+        this.u = d;
+        this.v = e;
+        this.w = f;
+    }
+
+    
+
+    /**
+     * @return the u
+     */
+    public int getU() {
+        return u;
+    }
+
+    /**
+     * @param u the u to set
+     */
+    public void setU(int u) {
+        this.u = u;
+    }
+
+    /**
+     * @return the v
+     */
+    public int getV() {
+        return v;
+    }
+
+    /**
+     * @param v the v to set
+     */
+    public void setV(int v) {
+        this.v = v;
+    }
+
+    /**
+     * @return the w
+     */
+    public int getW() {
+        return w;
+    }
+
+    /**
+     * @param w the w to set
+     */
+    public void setW(int w) {
+        this.w = w;
+    }
+}
---
+++ src/test_revisions/Test_Revisions.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test_revisions;
+
+/**
+ *
+ * @author Jacobiscoolio
+ */
+public class Test_Revisions {
+
+public Class2 class2;
+public Class3 class3;    
+
+
+public static void main(String[] args) {
+int[] array;
+String[] array1;
+boolean[] array2;
+array = new int[10];
+array1 = new String[10];
+array2 = new boolean[10];
+for (int i = 0;i < array.length; i++){
+           
+        }
+       
+    }
+    
+}
